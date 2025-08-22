2c281e3904808e09643df5ae1d4395898a2a5e4a
Abhinavpatra <149466436+Abhinavpatra@users.noreply.github.com>
2025-08-22 23:36:55 +0400

    name and runnable set

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
   return (
     <div className=" min-h-screen bg-[#272727] dark:bg-[#989898] text-[#807378] relative overflow-hidden bg-background flex items-center justify-center p-4">
        <h1 className="text-6xl md:text-8xl font-black mb-4 text-black dark:text-white">
-            COMING SOON...
-       </h1>
+CodeStreak.dev, coming soon.        
+      </h1>
     </div>
   );
 }
